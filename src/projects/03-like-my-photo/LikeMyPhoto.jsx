b5993269df1b38673f2b379ecfeb4c63e92af16f
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-21 23:41:59 -0500

    Added LikeMyPhoto starter file and connected it to App component.

---
+++ src/projects/03-like-my-photo/LikeMyPhoto.jsx
@@ -0,0 +1,9 @@
+import Title from '../../components/Title';
+
+export default function LikeMyPhoto() {
+  return (
+    <div className="container">
+      <Title text="Like My Photo" />
+    </div>
+  );
+}
