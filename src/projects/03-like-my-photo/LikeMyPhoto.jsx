a99985bff85e3f0bdaa4ab12fb4a489b2a455e6a
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-22 00:05:16 -0500

    Installed React Icons, then imported a few icons to be used in the card.

--- src/projects/03-like-my-photo/LikeMyPhoto.jsx
+++ src/projects/03-like-my-photo/LikeMyPhoto.jsx
@@ -1,4 +1,6 @@
 import Title from '../../components/Title';
+import { IoChatbubblesSharp } from "react-icons/io5";
+import { FaFaceSmile, FaHeart, FaRegHeart } from "react-icons/fa6";
 
 export default function LikeMyPhoto() {
   return (
@@ -6,11 +8,16 @@ export default function LikeMyPhoto() {
       <Title text="Like My Photo" />
       <Title classes="subtitle text-center" text="Likes" />
       <div className="card card-dark m-auto" style={{ width: 300, cursor: 'pointer'}}>
-        <div className="card-header fs-xl">
+        <div className="card-header fs-xl d-flex" style={{ justifyContent: 'space-between'}}>
+          <FaFaceSmile className="mr-2" />
           <small>Doggy</small>
         </div>
         <img src="#" style={{ height: "fit"}} />
-        <div className="card-footer fs-xl d-flex" style={{ justifyContent: 'space-between'}}></div>
+        <div className="card-footer fs-xl d-flex" style={{ justifyContent: 'space-between'}}>
+          <IoChatbubblesSharp />
+          <FaRegHeart />
+          <FaHeart />
+        </div>
       </div>
     </div>
   );
