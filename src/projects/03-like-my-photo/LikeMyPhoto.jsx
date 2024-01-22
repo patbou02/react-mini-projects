60d543185917c8146c63a81d9492fe6d65c90c42
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-21 23:49:26 -0500

    Created initial markup for the card with some styling.

--- src/projects/03-like-my-photo/LikeMyPhoto.jsx
+++ src/projects/03-like-my-photo/LikeMyPhoto.jsx
@@ -4,6 +4,14 @@ export default function LikeMyPhoto() {
   return (
     <div className="container">
       <Title text="Like My Photo" />
+      <Title classes="subtitle text-center" text="Likes" />
+      <div className="card card-dark m-auto" style={{ width: 300, cursor: 'pointer'}}>
+        <div className="card-header fs-xl">
+          <small>Doggy</small>
+        </div>
+        <img src="#" style={{ height: "fit"}} />
+        <div className="card-footer fs-xl d-flex" style={{ justifyContent: 'space-between'}}></div>
+      </div>
     </div>
   );
 }
