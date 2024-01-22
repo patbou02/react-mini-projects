9806433ef08e301e738a54fe7b4903fdbc1ddfce
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-22 00:24:19 -0500

    Added placeholder image. Set state for like and count and added handler to toggle like from true to false and counter add/substract based on the heart state.

--- src/projects/03-like-my-photo/LikeMyPhoto.jsx
+++ src/projects/03-like-my-photo/LikeMyPhoto.jsx
@@ -1,22 +1,32 @@
+import { useState } from 'react';
 import Title from '../../components/Title';
 import { IoChatbubblesSharp } from "react-icons/io5";
 import { FaFaceSmile, FaHeart, FaRegHeart } from "react-icons/fa6";
+import Frenchie from '../../images/frenchie.webp';
 
 export default function LikeMyPhoto() {
+
+  const [like, setLike] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
+
+  const handleHeartClick = () => {
+    setLike(!like);
+    setLikeCount(likeCount + (!like ? 1 : -1));
+  }
+
   return (
     <div className="container">
       <Title text="Like My Photo" />
-      <Title classes="subtitle text-center" text="Likes" />
-      <div className="card card-dark m-auto" style={{ width: 300, cursor: 'pointer'}}>
+      <Title classes="subtitle text-center" text={`Likes: ${likeCount}`} />
+      <div className="card card-dark m-auto shadow-md" style={{ width: 300}}>
         <div className="card-header fs-xl d-flex" style={{ justifyContent: 'space-between'}}>
           <FaFaceSmile className="mr-2" />
-          <small>Doggy</small>
+          <small>Frenchie</small>
         </div>
-        <img src="#" style={{ height: "fit"}} />
+        <img src={Frenchie} style={{ height: "fit"}} />
         <div className="card-footer fs-xl d-flex" style={{ justifyContent: 'space-between'}}>
           <IoChatbubblesSharp />
-          <FaRegHeart />
-          <FaHeart />
+          {like ? <FaHeart className="text-danger" style={{ cursor: 'pointer' }} onClick={handleHeartClick} /> : <FaRegHeart style={{ cursor: 'pointer' }} onClick={handleHeartClick} />}
         </div>
       </div>
     </div>
