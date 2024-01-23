d251ec97faff9440aaf740bf914c7b8d604b139c
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-23 00:02:41 -0500

    Created states and initial effect to run on initial render when contentType state gets updated. Created dummy API request.

--- src/projects/04-Testimonials/Testimonials.jsx
+++ src/projects/04-Testimonials/Testimonials.jsx
@@ -3,12 +3,21 @@ import Title from '../../components/Title';
 import { LiaCommentSolid } from "react-icons/lia";
 import { MdOutlinePostAdd } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
+import { useState, useEffect } from 'react';
 
 function Testimonials() {
 
-  const handlePostsButtonClick = () => console.log('clicked the posts button');
-  const handleUsersButtonClick = () => console.log('clicked the users button');
-  const handleCommentsButtonClick = () => console.log('clicked the comments button');
+  const [contentType, setContentType] = useState(null);
+
+  const API_REQUEST = `https://jsonplaceholder.typicode.com/${contentType}`;
+
+  useEffect(() => {
+    fetch(API_REQUEST).then(response => response.json()).then(json => console.log(json));
+  }, [ contentType ]);
+  
+  const handlePostsButtonClick = () => setContentType('posts');
+  const handleUsersButtonClick = () => setContentType('users');
+  const handleCommentsButtonClick = () => setContentType('comments');
 
   return (
     <div className="container m-auto">
@@ -16,6 +25,11 @@ function Testimonials() {
       <Button onClick={handlePostsButtonClick} icon={<MdOutlinePostAdd />} classes="btn-primary" text="Posts" />
       <Button onClick={handleUsersButtonClick} icon={<FaRegUser />} classes="btn-info" text="Users" />
       <Button onClick={handleCommentsButtonClick} icon={<LiaCommentSolid />} classes="btn-success" text="Comments" />
+      <Title text={contentType ? (contentType.charAt(0).toUpperCase() + contentType.substring(1)) : 'Select from above'} classes="subtitle text-primary" />
+      {contentType && (
+        <div>{contentType}</div>
+      )}
+
     </div>
   );
 }
