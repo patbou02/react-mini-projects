1f1acf8fbcf517999cca378f260fd3f2ae77ff3b
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-23 15:24:09 -0500

    Created structure of the alert element with support for type/message props and use of a Button for closing after a 1/2 sec delay. Created 3 Alert instances with different types and messages within the Alerts component.

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,8 +1,25 @@
 import { useState, useEffect } from 'react';
+import Button from '../components/Button';
+import { IoMdClose } from "react-icons/io";
 
-function Alert() {
+function Alert({ type, message }) {
+
+  const [showAlert, setShowAlert] = useState(true);
+
+  const closeAlert = e => {
+    e.target.parentElement.parentElement.parentElement.classList.add('fadeAlert');
+    setTimeout(()=> setShowAlert(false), 500);
+  }
+
   return (
-    <div>Alert</div>
+    showAlert && (
+      <div className={`alert alert-${type}`}>
+        <div className="alert-close">
+          <span className="mr-1">{message}</span>
+          <Button classes="btn-close" text="" icon={<IoMdClose />} onClick={closeAlert} />
+        </div>
+      </div>
+    )
   );
 }
 
--- src/projects/05-Alerts/Alerts.jsx
+++ src/projects/05-Alerts/Alerts.jsx
@@ -1,6 +1,15 @@
+import Alert from '../../components/Alert';
+
 function Alerts() {
 
-  return (<div className="container mx-auto">Alerts</div>);
+  return (
+    <div className="container mx-auto">
+      <h2 className="subtitle">Alerts</h2>
+      <Alert type="danger" message="Successful login" />
+      <Alert type="success" message="" />
+      <Alert type="info" message="" />
+    </div>
+  );
 }
 
 export default Alerts;
