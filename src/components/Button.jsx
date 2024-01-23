335c96458e2a46ef4d139022e37a4cf2359ecf2d
Patrick Boutin <patrick.boutin@roberthalf.com>
2024-01-22 23:31:22 -0500

    Added onClick prop to the Button component then created button handlers in the Testimonials component to be passed down.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
-function Button({ icon, classes, text }) {
+function Button({ icon, classes, text, onClick }) {
   const buttonStyles = {
     display: 'flex',
     alignItems: 'center',
   }
 
-  return <button styles={buttonStyles} className={`btn btn-lg ${classes}`}>{icon} {text ?? 'click'}</button>;
+  return <button onClick={onClick} styles={buttonStyles} className={`btn btn-lg ${classes}`}>{icon} {text ?? 'click'}</button>;
 }
 
 export default Button;
--- src/projects/04-Testimonials/Testimonials.jsx
+++ src/projects/04-Testimonials/Testimonials.jsx
@@ -5,12 +5,17 @@ import { MdOutlinePostAdd } from "react-icons/md";
 import { FaRegUser } from "react-icons/fa";
 
 function Testimonials() {
+
+  const handlePostsButtonClick = () => console.log('clicked the posts button');
+  const handleUsersButtonClick = () => console.log('clicked the users button');
+  const handleCommentsButtonClick = () => console.log('clicked the comments button');
+
   return (
     <div className="container m-auto">
       <Title text="Testimonials" />
-      <Button icon={<MdOutlinePostAdd />} classes="btn-primary" text="Posts" />
-      <Button icon={<FaRegUser />} classes="btn-info" text="Users" />
-      <Button icon={<LiaCommentSolid />} classes="btn-success" text="Comments" />
+      <Button onClick={handlePostsButtonClick} icon={<MdOutlinePostAdd />} classes="btn-primary" text="Posts" />
+      <Button onClick={handleUsersButtonClick} icon={<FaRegUser />} classes="btn-info" text="Users" />
+      <Button onClick={handleCommentsButtonClick} icon={<LiaCommentSolid />} classes="btn-success" text="Comments" />
     </div>
   );
 }
